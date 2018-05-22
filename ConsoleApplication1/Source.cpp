f33c69f85b83b23e1077e11afe1be0a78ed30a59
федоровк <@238_15>
2018-05-22 10:00:41 +0600

    Add project files.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int task;
+	cin >> task;
+	if (task == 1)
+	{
+		double x, y;
+		x = 3;
+		y = 3 * x*x - 7 * x + 4;
+		cout << y << endl;
+	}
+	if (task == 2)
+	{
+		int x, a;
+		a = 4;
+		x=12 * a*a + 7 * a - 16;
+		cout << x << endl;
+
+	}
+	if (task == 3)
+	{
+		int a,p;
+		a = 5;
+		p = 4 * a;
+
+
+	}
+	
+
+}
